44dbe1b7e3ce9b469dd4733fbfd7bd642b128707
Mariah <mariahmaic23@gmail.com>
2022-11-22 19:26:31 -0600

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :orders
   resources :users
   resources :products
+
+  root to: "main#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
